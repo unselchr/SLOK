7e7bbaa52b9a122dcd977c66ef2f4b0e7b5eb854
unselchr <cdojrf@gmail.com>
2018-08-07 13:38:53 -0500

    moved old stuff to old and added dotenv

---
+++ old/server.js
@@ -0,0 +1,36 @@
+var express=require("express");
+var path=require("path");
+var bodyParser=require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+///////////////////////////////////
+app.get("/",function(req,res){
+    console.log(req.query);
+    res.sendFile(path.join(__dirname,"assets/html/index.html"));
+});
+app.get("/assets/pictures/Fallerein_Party.png",function(req,res){
+    res.sendFile(path.join(__dirname,"assets/pictures/Fallerein_Party.png"));
+});
+app.get("/assets/styles/headerCustom.css",function(req,res){
+    res.sendFile(path.join(__dirname,"/assets/styles/headerCustom.css"));
+});
+app.get("/assets/styles/default.css",function(req,res){
+    res.sendFile(path.join(__dirname,"assets/styles/default.css"));
+});
+app.get("/assets/js/accountSystem.js",function(req,res){
+    res.sendFile(path.join(__dirname,"assets/js/default.js"));
+});
+app.get("/assets/js/accountSystem.js",function(req,res){
+    res.sendFile(path.join(__dirname,"assets/js/default.js"));
+});
+app.get("/assets/pictures/background.png",function(req,res){
+    res.sendFile(path.join(__dirname,"assets/pictures/background.png"));
+});
+///////////////////////////////////
+app.listen(PORT,function(){
+    console.log("Listening on PORT: "+PORT);
+})
--- server.js
+++ server.js
@@ -1,36 +1,7 @@
 var express=require("express");
 var path=require("path");
 var bodyParser=require("body-parser");
+require('dotenv').config()
 
 var app = express();
 var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-///////////////////////////////////
-app.get("/",function(req,res){
-    console.log(req.query);
-    res.sendFile(path.join(__dirname,"assets/html/index.html"));
-});
-app.get("/assets/pictures/Fallerein_Party.png",function(req,res){
-    res.sendFile(path.join(__dirname,"assets/pictures/Fallerein_Party.png"));
-});
-app.get("/assets/styles/headerCustom.css",function(req,res){
-    res.sendFile(path.join(__dirname,"/assets/styles/headerCustom.css"));
-});
-app.get("/assets/styles/default.css",function(req,res){
-    res.sendFile(path.join(__dirname,"assets/styles/default.css"));
-});
-app.get("/assets/js/accountSystem.js",function(req,res){
-    res.sendFile(path.join(__dirname,"assets/js/default.js"));
-});
-app.get("/assets/js/accountSystem.js",function(req,res){
-    res.sendFile(path.join(__dirname,"assets/js/default.js"));
-});
-app.get("/assets/pictures/background.png",function(req,res){
-    res.sendFile(path.join(__dirname,"assets/pictures/background.png"));
-});
-///////////////////////////////////
-app.listen(PORT,function(){
-    console.log("Listening on PORT: "+PORT);
-})
